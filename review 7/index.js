9c975ef8a5e50fe99f908d547ddd38d42dbbb546
sionesmith <smithsione.s91@gmail.com>
2024-10-17 14:01:03 +1300

    review7

---
+++ review 7/index.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("button");
+const container = document.getElementById("container");
+button.addEventListener('click',
+function(){
+    alert("you clicked me");
+});
+
+function change(){
+    blue.style.backgroundColor="blue";
+    console.log("blue");
+}
+const blue = document.getElementById("blue");
+blue.style.backgroundColor="blue";
